5386f356fe205a63191266ea9505eed2883dd2a3
Jordan Gray <business@jordantgray.uk>
2024-10-30 10:16:01 +0000

    init

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        
+        ToolTipProperties.SetText(CounterBtn, count.ToString());
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
